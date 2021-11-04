78c9d1b323a6346ef959a3ba0c9bd42f835705a1
adrianglezlop <entornoc9@gmail.com>
2021-11-04 17:41:15 +0000

    quitando migrate

--- db/migrate/20211025151904_add_cred_o_vale_to_credits.rb
+++
@@ -1,5 +0,0 @@
-class AddCredOValeToCredits < ActiveRecord::Migration
-  def change
-    add_column :credits, :cred_o_vale, :string
-  end
-end
--- db/migrate/20211026181744_add_cred_vale_columns_to_credits.rb
+++
@@ -1,5 +0,0 @@
-class AddCredValeColumnsToCredits < ActiveRecord::Migration
-  def change
-    add_column :credits, :credvale, :string
-  end
-end
